46bf69c3ccc624bcf4033336820b2c2a2e38335a
ElectricGrasshopper <113239307+ElectricGrasshopper@users.noreply.github.com>
2022-10-22 17:56:46 +0530

    add gsap animation components

---
+++ src/animation/AnimateInOut.tsx
@@ -0,0 +1,41 @@
+import React, { useRef, useContext } from "react"
+import { gsap } from "gsap"
+import { Box } from "theme-ui"
+import useIsomorphicLayoutEffect from "./useIsomorphicLayoutEffect"
+import { TransitionContext } from "../context/TransitionContext"
+
+const AnimateInOut = ({
+  children,
+  as,
+  from,
+  to,
+  durationIn,
+  durationOut,
+  delay,
+  delayOut,
+  set,
+  skipOutro,
+}) => {
+  const timeline = useContext(TransitionContext)
+  const el = useRef()
+
+  useIsomorphicLayoutEffect(() => {
+    // intro animation
+    if (set) {
+      gsap.set(el.current, { ...set })
+    }
+    gsap.to(el.current, {
+      ...to,
+      delay: delay || 0,
+      duration: durationIn,
+    })
+  }, [])
+
+  return (
+    <Box as={as} sx={from} ref={el}>
+      {children}
+    </Box>
+  )
+}
+
+export default React.memo(AnimateInOut)
